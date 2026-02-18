8311ce8c98f30f999e308d356bc2876e8c1a1018
MarkManFlame55 <marcos.mananesruiz@educa.madrid.org>
2026-02-18 18:35:08 +0100

    Importar los DTOS + Config de Java

--- server/BomboplatsServer/src/main/java/org/dam2/bomboplatsserver/BomboplatsServerApplication.java
+++ server/BomboplatsServer/src/main/java/org/dam2/bomboplatsserver/BomboplatsServerApplication.java
@@ -1,5 +1,6 @@
 package org.dam2.bomboplatsserver;
 
+import org.dam2.bomboplats.api.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
