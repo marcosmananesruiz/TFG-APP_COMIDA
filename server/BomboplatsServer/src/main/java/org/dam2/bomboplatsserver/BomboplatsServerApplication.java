98d9efa97ea4bffc595da51a64e9867a4620c665
MarkManFlame55 <marcos.mananesruiz@educa.madrid.org>
2026-02-18 18:10:43 +0100

    Init Server

---
+++ server/BomboplatsServer/src/main/java/org/dam2/bomboplatsserver/BomboplatsServerApplication.java
@@ -0,0 +1,13 @@
+package org.dam2.bomboplatsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomboplatsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BomboplatsServerApplication.class, args);
+    }
+
+}
---
+++ server/BomboplatsServer/src/test/java/org/dam2/bomboplatsserver/BomboplatsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dam2.bomboplatsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomboplatsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
