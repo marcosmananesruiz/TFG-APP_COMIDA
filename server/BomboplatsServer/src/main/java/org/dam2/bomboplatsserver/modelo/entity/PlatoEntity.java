e1596b03fce051f53d2b614ee7b81e7d2adf3ead
jcaceresforteza <145029840+jcaceresforteza@users.noreply.github.com>
2026-02-20 13:58:17 +0100

    atributo de prueba

--- server/BomboplatsServer/src/main/java/org/dam2/bomboplatsserver/modelo/entity/PlatoEntity.java
+++ server/BomboplatsServer/src/main/java/org/dam2/bomboplatsserver/modelo/entity/PlatoEntity.java
@@ -28,6 +28,7 @@ public class PlatoEntity {
 
     private String idRestaurante; //relacion con RestauranteEntity, Length = 8. Borrar los platos no borrara los restaurantes.
 
+    private String polla;
 
 
 }
