25503e257644c124951d3ce12a5f0dbd1eae3e6b
marcos.mananesruiz <marcos.mananesruiz@educa.madrid.org>
2026-05-05 19:04:30 +0200

    Logs para errores en S3

--- server/BomboplatsServer/src/main/java/org/dam2/bomboplatsserver/service/impl/S3ServiceImpl.java
+++ server/BomboplatsServer/src/main/java/org/dam2/bomboplatsserver/service/impl/S3ServiceImpl.java
@@ -1,6 +1,8 @@
 package org.dam2.bomboplatsserver.service.impl;
 
 import org.dam2.bomboplatsserver.service.IS3Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,7 @@ public class S3ServiceImpl implements IS3Service {
     @Autowired private S3Presigner presigner;
     @Value("${aws.s3.bucket}") private String bucket;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(S3ServiceImpl.class);
 
     @Override
     public Mono<String> generateUpdloadUrl(String key) {
@@ -40,13 +43,15 @@ public class S3ServiceImpl implements IS3Service {
                     presigner.presignPutObject(presignRequest);
 
             return presignedRequest.url().toString();
-        }).subscribeOn(Schedulers.boundedElastic());
+        }).subscribeOn(Schedulers.boundedElastic())
+        .doOnError(e -> LOGGER.error("ERROR generando Presigned URL para Bucket S3 {}: {}", this.bucket, e.getMessage()));
     }
 
-
+    @Override
     public Mono<String> generateUserIconUrl(String userId) {
         return this.generateUpdloadUrl("profile/" + userId + ".jpg");
     }
+
     @Override
     public Mono<String> generateRestauranteIconUrl(String restauranteId, int index) {
         return this.generateUpdloadUrl("restaurantes/" + restauranteId + "_" + index + ".jpg");
