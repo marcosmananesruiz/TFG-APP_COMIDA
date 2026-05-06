c038588dde33ea1feff50d23bc6d993252d07ea9
albertoCatel <alberto.catel@educa.madrid.org>
2026-05-06 16:50:16 +0200

    he borrado la foto de IA voy en el fragment de registrarse y he puesto un regex para que el correo introducido sea valido

--- frontend/Bomboplats/app/src/main/java/com/example/bomboplats/ui/login/RegisterActivity.java
+++ frontend/Bomboplats/app/src/main/java/com/example/bomboplats/ui/login/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.bomboplats.ui.login;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -78,6 +79,12 @@ public class RegisterActivity extends AppCompatActivity {
         if (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || 
             TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPassword)) {
             Toast.makeText(this, getString(R.string.toast_rellenar_campos), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Validación de formato de email mediante Regex (Patterns.EMAIL_ADDRESS)
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, getString(R.string.invalid_username), Toast.LENGTH_SHORT).show();
             return;
         }
 
