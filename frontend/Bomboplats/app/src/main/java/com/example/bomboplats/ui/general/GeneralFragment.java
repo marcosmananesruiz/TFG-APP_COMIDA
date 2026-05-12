2024d40a6d528c701506b45a334efa89de7a1760
marcos.mananesruiz <marcos.mananesruiz@educa.madrid.org>
2026-05-12 20:14:04 +0200

    Los restaurantes se filtran por nombre

--- frontend/Bomboplats/app/src/main/java/com/example/bomboplats/ui/general/GeneralFragment.java
+++ frontend/Bomboplats/app/src/main/java/com/example/bomboplats/ui/general/GeneralFragment.java
@@ -19,6 +19,8 @@ import com.example.bomboplats.data.model.Restaurante;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -61,6 +63,7 @@ public class GeneralFragment extends Fragment implements RestauranteAdapter.OnRe
 
             if (restaurantes != null && !restaurantes.isEmpty()) {
                 listaCompleta = restaurantes;
+                listaCompleta.sort(Comparator.comparing(Restaurante::getNombre));
                 adapter.setFilteredList(new ArrayList<>(listaCompleta));
                 recyclerView.setVisibility(View.VISIBLE);
                 tvEmptyError.setVisibility(View.GONE);
